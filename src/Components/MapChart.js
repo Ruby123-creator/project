7e805a98a15a80f6b5fcc644a18a29f8e54c1772
Your Name <you@example.com>
2023-06-18 22:40:26 +0530

    color chnage

--- src/Components/MapChart.js
+++ src/Components/MapChart.js
@@ -7,19 +7,19 @@ import axios from 'axios';
 import mapData from './../Data/countries.json'
 import Country from './CountryDetails/Country';
 const countryStyle = {
-  fillColor: "red",
+  fillColor: "#9E072E",
   fillOpacity: 1,
   color: "black",
   weight: 1,
   dashArray:5,
 };
 const MapChart = () => {
-  const [color ,setcolor] = useState("#ffff00")
   const [searchCountry ,setSearchCountry] = useState('')
  const [selectedCountry ,setSelectedCountry] = useState(null)
  const [countryDetails ,setCountryDetails] = useState(null)
   useEffect(() => {
     if (selectedCountry) {
+      console.log("hello")
       fetchCountryDetails(selectedCountry);
     }
   }, [selectedCountry]);
@@ -27,10 +27,10 @@ const MapChart = () => {
 
 
   const fetchCountryDetails = async (countryName) => {
+    console.log("hello")
     try {
       const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
-      // const data = await response.json();
-      // console.log(data)
+      console.log(response.data)
       setCountryDetails(response.data);
     } catch (error) {
       alert("No country Found")
@@ -40,22 +40,16 @@ const MapChart = () => {
 
 
  const  handleSearch =(e)=>{
+
    e.preventDefault()
+  //  console.log("hello") 
+
     fetchCountryDetails(searchCountry)
  }
  
- const changeCountryColor = (event) => {
-    event.target.setStyle({
-      color: "green",
-      fillColor: color,
-      fillOpacity: 1,
-    });
-  };
-  const handleCountryClick=(event )=>{    
-      
-
-    const countryCode = event.target.feature.properties.ADMIN
-    setSelectedCountry(countryCode)
+ 
+  const handleCountryClick=(countryName)=>{
+   setSelectedCountry(countryName)
     
   }
  const onEachCountry = (country, layer ) => {
@@ -65,22 +59,18 @@ const MapChart = () => {
     layer.options.fillOpacity = Math.random(); 
      
     layer.on({
-      click: handleCountryClick,
+      click:()=>handleCountryClick(countryName),
     });
   };
-//  const colorChange = (event) => {
-//   setcolor(event.target.value)
-//   };
-  
-  // console.log(countries)
+
   return (
-    <div style={{display:'flex' ,flexWrap:'wrap'}} >
+    <div style={{display:'flex' ,flexWrap:'wrap', alignItems:'center' ,justifyContent:'center' ,gap:'5'}} >
     <div style={{width:'70vw'}}>
-        <div>
-          <input type="search" name="" id="" value={searchCountry} onChange={(e)=>setSearchCountry(e.target.value)} />
-          <button type="submit" onClick={handleSearch}>submit</button>
+        <div className='searchbox'>
+          <input type="search" placeholder='Enter Country Name' className='inputbox' value={searchCountry} onChange={(e)=>setSearchCountry(e.target.value)} />
+          <button type="submit" onClick={handleSearch}>search</button>
         </div>
-       <MapContainer zoom={2} center={[20,100]} style={{height:'80vh'}}>
+       <MapContainer zoom={2} center={[20,30]} style={{height:'80vh'}}>
        <GeoJSON
             style={countryStyle}
             data={mapData.features}
@@ -88,16 +78,12 @@ const MapChart = () => {
           
 
           />
-          {/* <input type="color" 
-           value={color}
-           onChange={colorChange}
-
-           /> */}
+          
        </MapContainer>
        </div>
        <div>
         {
-          selectedCountry && <Country  details={countryDetails}/>
+          countryDetails && <Country  details={countryDetails}/>
         }
        </div>
     </div>
