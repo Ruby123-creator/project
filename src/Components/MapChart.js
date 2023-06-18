69f7fe23abf211ca28c1260f86be10fa71fa20a2
Your Name <you@example.com>
2023-06-19 00:23:38 +0530

    chnage countrycolor

--- src/Components/MapChart.js
+++ src/Components/MapChart.js
@@ -6,6 +6,8 @@ import './MapChart.css'
 import axios from 'axios';
 import mapData from './../Data/countries.json'
 import Country from './CountryDetails/Country';
+
+//styles which provided to the GeoJson element
 const countryStyle = {
   fillColor: "#9E072E",
   fillOpacity: 1,
@@ -14,16 +16,22 @@ const countryStyle = {
   dashArray:5,
 };
 const MapChart = () => {
+  const [color ,setColor] = useState('yellow')
+  // state to change the serach country
   const [searchCountry ,setSearchCountry] = useState('')
+  //state changes whenever u selected country
  const [selectedCountry ,setSelectedCountry] = useState(null)
+ // to update the country details and set the details
  const [countryDetails ,setCountryDetails] = useState(null)
+ // to store the cache info whenever the user selected same country
  const [cache, setCache] = useState({});
 
  const cachedData = useMemo(() => cache[selectedCountry], [cache, selectedCountry]);
 
   useEffect(() => {
+    //whenever the user click inside the country boundary 
     if (selectedCountry) {
-      console.log("hello")
+      //fetch the country details
       fetchCountryDetails(selectedCountry);
     }
   }, [selectedCountry]);
@@ -31,12 +39,15 @@ const MapChart = () => {
 
 
   const fetchCountryDetails = async (countryName) => {
+    // if countryName is same as in the cacheobject then the data fetch from cache
     if (cache[countryName]) {
       return;
     }
+
     try {
       const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
-      console.log(response.data)
+      // console.log(response.data)
+      //create a chache object
       setCache((prevCache) => ({
         ...prevCache,
         [countryName]: response.data,
@@ -51,26 +62,38 @@ const MapChart = () => {
 
 
  const  handleSearch =(e)=>{
-
+   
    e.preventDefault()
   //  console.log("hello") 
-
+  
+  // wehenver serach btn click it fetch the data 
     fetchCountryDetails(searchCountry)
  }
  
  
-  const handleCountryClick=(countryName)=>{
+  const handleCountryClick=(e,countryName)=>{
+
+    // to change the color of selected country
+    e.target.setStyle({
+      fillColor:color,
+      fillOpacity:1
+    })
+    // console.log(e.target.feature.properties)
    setSelectedCountry(countryName)
     
   }
  const onEachCountry = (country, layer ) => {
+  // get the country name from json file
   const countryName = country.properties.ADMIN;
+  // used to popup the countryname whenever it hits the layer boundaries
   layer.bindPopup(countryName);
     
-    layer.options.fillOpacity = Math.random(); 
-     
+  // this is used to change the different opacity for different countries
+    layer.options.fillOpacity = Math.random(); //0-1
+    
+    //whenever the user click on the particular country area it fetchs the details of that particular country
     layer.on({
-      click:()=>handleCountryClick(countryName),
+      click:(e)=>handleCountryClick(e,countryName),
     });
   };
 
@@ -84,16 +107,22 @@ const MapChart = () => {
        <MapContainer zoom={2} center={[20,30]} style={{height:'80vh'}}>
        <GeoJSON
             style={countryStyle}
+        
             data={mapData.features}
             onEachFeature={onEachCountry}
           
 
           />
-          
+          <input
+          type="color"
+          value={color}
+          onChange={(e)=>setColor(e.target.value)}
+        />
        </MapContainer>
        </div>
        <div>
         {
+          // whenver the country details fetched or true then call the country componnet
           countryDetails && <Country  details={countryDetails}/>
         }
        </div>
